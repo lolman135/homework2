23d78ab437b0a2e62cbb70896b9f17686f84c723
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-16 23:34:11 +0300

    Add kms to miles converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
 public class Main {
+    private static final double COEF_M = 1.60934;
     public static void main(String[] args) {
         System.out.println("Kilometers to Miles converter");
+    }
+    private static double kmsToMiles(double kms){
+        return kms / COEF_M;
     }
 }

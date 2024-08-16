9c6a3c8a914693a0ab62c7c027565da4e47f09f2
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-16 23:45:05 +0300

    Add convertion to main method

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,11 @@ package app;
 public class Main {
     private static final double COEF_M = 1.60934;
     public static void main(String[] args) {
-        System.out.println("Kilometers to Miles converter");
+        System.out.println("Kilometers to Miles converter\n" +
+                "===============================");
+        double kms = 10, miles = 10;
+        System.out.println(kms + " kilometers: " + kmsToMiles(kms) + " miles");
+        System.out.println(miles + " miles: " + milesToKms(miles) + " kilometers");
     }
     private static double kmsToMiles(double kms){
         return kms / COEF_M;

fb160394a50d2f239ca3f8881e690fa381c95990
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-16 23:50:34 +0300

    Update Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 public class Main {
     private static final double COEF_M = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("Kilometers to Miles converter\n" +
                 "===============================");
@@ -9,9 +10,11 @@ public class Main {
         System.out.println(kms + " kilometers: " + kmsToMiles(kms) + " miles");
         System.out.println(miles + " miles: " + milesToKms(miles) + " kilometers");
     }
+
     private static double kmsToMiles(double kms){
         return kms / COEF_M;
     }
+
     private static double milesToKms(double miles){
         return miles * COEF_M;
     }
